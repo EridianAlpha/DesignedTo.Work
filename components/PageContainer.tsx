efdf5c49726bdee160e0719b3aba78b1c172cbe1
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-12-11 17:20:38 +0000

    Add sparkle button example

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -4,6 +4,7 @@ import { Box, VStack } from "@chakra-ui/react"
 import DesignContainer from "./DesignContainer"
 import RotatingText from "./designs/rotating-text/RotatingText"
 import DragLockDirection from "./designs/drag-lock-direction/DragLockDirection"
+import SparkleButton from "./designs/sparkle-button/SparkleButton"
 
 export default function PageContainer() {
     return (
@@ -11,9 +12,14 @@ export default function PageContainer() {
             <RotatingText />
             <Box h={"30px"} />
             <DesignContainer>
+                <SparkleButton />
+            </DesignContainer>
+            <DesignContainer>
                 <DragLockDirection />
             </DesignContainer>
-            <DesignContainer>{/* <RotatingText /> */}</DesignContainer>
+            <DesignContainer>
+                <RotatingText />
+            </DesignContainer>
             <Box h={"20px"} />
         </VStack>
     )
