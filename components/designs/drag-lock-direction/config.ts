2debdafbaae8fa4ba4d89875ef886f102d511c89
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2026-01-10 17:10:51 +0000

    Update source link

--- components/designs/drag-lock-direction/config.ts
+++ components/designs/drag-lock-direction/config.ts
@@ -1,6 +1,6 @@
 export const dragLockDirectionConfig = {
     slug: "drag-lock-direction",
     title: "Drag Lock Direction",
-    sourceLink: "https://motion.dev/tutorials/react-drag",
+    sourceLink: "https://motion.dev/examples/react-drag-lock-direction",
     designNotesLink: "https://docs.eridian.xyz/design-dev/designedto.work/drag-lock-direction",
 } as const
